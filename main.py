fa0ce3fafcbfaaf27835740ed76fc32ed2dfeb0e
Taylor Allen <taylorallen0913@gmail.com>
2022-11-01 16:46:44 -0700

    done

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 # ******************************
 # Make your Code
 # ******************************
+
+numbers = list(map(int, input().split()))
+
+for i in range(len(numbers)):
+    # Find min index
+    min_index = numbers.index(min(numbers[i:]))
+    # Swap
+    temp = numbers[i]
+    numbers[i] = numbers[min_index]
+    numbers[min_index] = temp
+
+    print(numbers)
+  
